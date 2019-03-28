dfbe35a687f9af95f28ea1a7885806203be0a45e
Kyle-Jensen <46944266+Kyle-Jensen@users.noreply.github.com>
2019-03-28 18:10:18 -0500

    form complete

--- Assignments/Assignment7_5/bBall/bBall/Form1.Designer.cs
+++ Assignments/Assignment7_5/bBall/bBall/Form1.Designer.cs
@@ -28,23 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.teamslistBox = new System.Windows.Forms.ListBox();
+            this.winnersLabel = new System.Windows.Forms.Label();
+            this.ClearButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // listBox1
+            // teamslistBox
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(0, 0);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(120, 95);
-            this.listBox1.TabIndex = 0;
+            this.teamslistBox.FormattingEnabled = true;
+            this.teamslistBox.Location = new System.Drawing.Point(12, 12);
+            this.teamslistBox.Name = "teamslistBox";
+            this.teamslistBox.Size = new System.Drawing.Size(104, 420);
+            this.teamslistBox.TabIndex = 0;
+            // 
+            // winnersLabel
+            // 
+            this.winnersLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.winnersLabel.Location = new System.Drawing.Point(122, 193);
+            this.winnersLabel.Name = "winnersLabel";
+            this.winnersLabel.Size = new System.Drawing.Size(286, 23);
+            this.winnersLabel.TabIndex = 1;
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.Location = new System.Drawing.Point(228, 231);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(75, 23);
+            this.ClearButton.TabIndex = 2;
+            this.ClearButton.Text = "Clear";
+            this.ClearButton.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.listBox1);
+            this.ClientSize = new System.Drawing.Size(415, 450);
+            this.Controls.Add(this.ClearButton);
+            this.Controls.Add(this.winnersLabel);
+            this.Controls.Add(this.teamslistBox);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -53,7 +74,9 @@
 
         #endregion
 
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox teamslistBox;
+        private System.Windows.Forms.Label winnersLabel;
+        private System.Windows.Forms.Button ClearButton;
     }
 }
 
