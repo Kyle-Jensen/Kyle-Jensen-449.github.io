faae852604a44dd908094b71224d5a2b0a092b0d
Kyle-Jensen <46944266+Kyle-Jensen@users.noreply.github.com>
2019-04-04 10:32:17 -0500

    added more strings

--- Assignments/Vending Machine/Vending Machine/Form1.cs
+++ Assignments/Vending Machine/Vending Machine/Form1.cs
@@ -27,6 +27,12 @@ namespace Vending_Machine
             InitializeComponent();
         }
 
+        string[,] soda = new string[,] { {"Cola", "1.00", "20"}, { "Root Beer", "1.00", "20" }, { "Lemon Lime", "1.00", "20" }, { "Grape Soda", "1.50", "20" }, { "Cream Soda", "1.50", "20" } };
+
+        double total_sales = 0.00;
+        inputDrinks entry = new inputDrinks();
+        int index;
+
         private void vendingMachine_Load(object sender, EventArgs e)
         {
 
