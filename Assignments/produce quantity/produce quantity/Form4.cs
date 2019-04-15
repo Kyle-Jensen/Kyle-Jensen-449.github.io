179514b64983306bc9520b21c63df5e363aef68e
Kyle-Jensen <46944266+Kyle-Jensen@users.noreply.github.com>
2019-04-14 19:44:16 -0500

    started a new form for item info

---
+++ Assignments/produce quantity/produce quantity/Form4.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using produce_quantity.Objectssssss;
+namespace produce_quantity
+{
+    public partial class Form4 : Form
+    {
+        GroceryItem _GroceryItem;
+        public Form4(GroceryItem groceryItem)
+        {
+            _GroceryItem = groceryItem;
+            InitializeComponent();
+
+            
+
+        }
+    }
+}
